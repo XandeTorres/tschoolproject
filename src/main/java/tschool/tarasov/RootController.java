62fd94271e3d8bdb19866553188080c7f8a0e896
XandeTorres <aleks.tars@mail.ru>
2021-10-06 11:54:08 +0300

    democontroller and view were edited

--- src/main/java/tschool/tarasov/RootController.java
+++ src/main/java/tschool/tarasov/RootController.java
@@ -5,12 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class RootController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/greetingp")
+    public String greetingp(HttpServletRequest request) {
+        String name = request.getParameter("n");
+        String surname = request.getParameter("s");
+        System.out.println(name + " " + surname);
+
         return "greeting";
     }
 }
