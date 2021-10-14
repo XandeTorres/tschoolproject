642c71d0848e8002d9735099ce2f9b2730388203
XandeTorres <aleks.tars@mail.ru>
2021-10-15 01:53:55 +0300

    add template code for setting index.html as default. need to fix

--- src/main/java/tschool/tarasov/config/SpringConfig.java
+++ src/main/java/tschool/tarasov/config/SpringConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -55,4 +56,13 @@ public class SpringConfig implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
+    /**
+     * addViewController need to set up index.html instead default index.jsp
+     */
+    // TODO: need to set up as soon as possible
+    //@Override
+    //public void addViewControllers(ViewControllerRegistry registry) {
+    //    registry.addViewController("/").setViewName("forward:/index.html");
+    //}
 }
