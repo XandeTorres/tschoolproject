afa03b61cde5eac7972c4de1b8065ad8217975b7
XandeTorres <aleks.tars@mail.ru>
2021-10-15 01:54:45 +0300

    add filter of hidden HTTP fields

--- src/main/java/tschool/tarasov/config/MobileOperatorSpringMvcDispatcherServletInitializer.java
+++ src/main/java/tschool/tarasov/config/MobileOperatorSpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,10 @@
 package tschool.tarasov.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * This class used instead web.xml. AbstractAnnotationConfigDispatcherServletInitializer is subclass of
@@ -23,5 +27,19 @@ public class MobileOperatorSpringMvcDispatcherServletInitializer extends Abstrac
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    /**
+     * Filter of hidden HTTP fields that registering with app start
+     */
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
