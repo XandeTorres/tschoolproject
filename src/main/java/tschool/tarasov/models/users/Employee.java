79c5e610f93b016315e5da4b5211973ac49a6224
XandeTorres <aleks.tars@mail.ru>
2021-10-20 00:24:34 +0300

    one more entities added

---
+++ src/main/java/tschool/tarasov/models/users/Employee.java
@@ -0,0 +1,10 @@
+package tschool.tarasov.models.users;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Employee extends User{
+
+}
