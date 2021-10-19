2dc1f3e11f2ee4b8fdfbbfaf18fb90883f0c3e32
XandeTorres <aleks.tars@mail.ru>
2021-10-20 00:23:23 +0300

    DAO classes added

---
+++ src/main/java/tschool/tarasov/dao/ContractDao.java
@@ -0,0 +1,48 @@
+package tschool.tarasov.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import tschool.tarasov.models.Contract;
+
+
+import javax.persistence.Query;
+import java.util.List;
+
+
+@Repository
+public class ContractDao {
+
+    private final SessionFactory sessionFactory;
+    @Autowired
+    public ContractDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void createContract(Contract contract) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(contract);
+    }
+
+    public Contract getContractById(Long id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Contract contract = (Contract) session.load(Contract.class, id);
+        return contract;
+    }
+
+    public Contract getContractByNumber(String number) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Query query =session.createQuery("FROM Contract WHERE number = :number").setParameter("number", number);
+        Contract contract = (Contract) query.getSingleResult();
+        return contract;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Contract> getContracts() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Contract> listContracts = session.createQuery("from Contract ").list();
+        //logger.info("Getting list of Customers successfully");
+        return listContracts;
+    }
+}
---
+++ src/main/java/tschool/tarasov/dao/CustomerDao.java
@@ -0,0 +1,59 @@
+package tschool.tarasov.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import tschool.tarasov.models.users.Customer;
+
+import java.util.List;
+
+@Repository
+public class CustomerDao {
+
+    //private static final Logger logger = LoggerFactory.getLogger(CustomerDao.class);
+
+    private final SessionFactory sessionFactory;
+    @Autowired
+    public CustomerDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    public void createCustomer(Customer customer) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(customer);
+        //logger.info("Customer was saved to DB successfully" + customer.getPassport());
+
+    }
+
+    public void updateCustomer(Customer customer) {
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(customer);
+        //logger.info("Customer was updated successfully. Passport: " + customer.getPassport());
+    }
+
+    public Customer getCustomerById(Long id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Customer customer = (Customer) session.load(Customer.class, id);
+       // logger.info("Customer was loaded. Id: " + customer.getId());
+        return customer;
+    }
+    @SuppressWarnings("unchecked")
+    public List<Customer> getCustomers() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Customer> listCustomers = session.createQuery("from Customer").list();
+        //logger.info("Getting list of Customers successfully");
+        return listCustomers;
+    }
+
+
+
+
+
+
+
+
+}
